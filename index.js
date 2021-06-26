8575176eec267401d2f7e2aaca88f1072c364771
mneser <morne.neser@gmail.com>
2021-06-26 03:29:14 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const fetch = require('node-fetch')
-const express = require('express')
-const app = express()
-
-app.get('/code', (req, res) => res.send(fs.readFileSync('debug.js', 'utf8')))
-app.get('/git', (req, res) => fetch('https://github.com/mneser/debugger').then(rs => rs.text()).then(tx => res.send(tx)))
-
-app.listen(80, () => console.log('Server running on localhost'))
